badac80c4a8cf0d49f45481ec2ef6d9531857dad
pure-hate <chuchundra06@gmail.com>
2021-08-07 16:48:28 +0300

    added rules for battleobjetcs, delete check battleitem and added checktile for battleobject in hacking state, small fix in SaveBattleGame.cpp

--- src/Savegame/SavedBattleGame.cpp
+++ src/Savegame/SavedBattleGame.cpp
@@ -316,7 +316,7 @@ void SavedBattleGame::load(const YAML::Node &node, Mod *mod, SavedGame* savedGam
 		_battleObjects.push_back(object);
 		Position pos = (*i)["position"].as<Position>(Position(-1, -1, -1));
 		if (pos.x != -1)
-			getTile(pos)->setObject(object);
+			getTile(pos)->setBattleObject(object);
 
 	}
 
