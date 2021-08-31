11707acf4eec196a3968d3662d8db4feb2ad1f02
pure-hate <37094875+pure-hate@users.noreply.github.com>
2021-08-31 22:23:07 +0300

    Merge with Hacking tool second (#86)
    hackObject() function changes tiles in a set radius around Battle Object to their alternative variants. It can be used to open locked doors or making other changes to the map, that are triggered by hacking a Battle object
    
    * moving iterateTiles back to tileengine.cpp, implenent successfulHack and move this to tileEngine
    
    * moving code to hackObject, cleaning function
    
    * delete some whitespace, renaming vars

--- src/Battlescape/TileEngine.cpp
+++ src/Battlescape/TileEngine.cpp
@@ -160,6 +160,32 @@ bool calculateLineHelper(const Position& origin, const Position& target, FuncNew
 	return false;
 }
 
+template<typename TileFunc>
+void iterateTiles(SavedBattleGame* save, MapSubset gs, TileFunc func)
+{
+	const auto totalSizeX = save->getMapSizeX();
+	const auto totalSizeY = save->getMapSizeY();
+	const auto totalSizeZ = save->getMapSizeZ();
+
+	gs = MapSubset::intersection(gs, MapSubset{ totalSizeX, totalSizeY });
+	if (gs)
+	{
+		for (int z = 0; z < totalSizeZ; ++z)
+		{
+			auto rowStart = save->getTile(Position{ gs.beg_x, gs.beg_y, z });
+			for (auto stepsY = gs.size_y(); stepsY != 0; --stepsY, rowStart += totalSizeX)
+			{
+				auto curr = rowStart;
+				for (auto stepX = gs.size_x(); stepX != 0; --stepX, curr += 1)
+				{
+					func(curr);
+				}
+			}
+		}
+	}
+}
+
+
 template<typename FuncNewPosition>
 bool calculateParabolaHelper(const Position& origin, const Position& target, double curvature, const Position& delta, FuncNewPosition posFunc)
 {
