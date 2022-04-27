1b987387781f2aba972ea11724500f2533d641bd
Finnik <45336117+Finnik723@users.noreply.github.com>
2022-04-28 02:08:01 +0300

    role mechanics for dogfight

--- src/Mod/RuleSoldier.cpp
+++ src/Mod/RuleSoldier.cpp
@@ -270,6 +270,7 @@ void RuleSoldier::afterLoad(const Mod* mod)
 
 void RuleSoldier::loadRoles(const std::vector<int> &r)
 {
+	_roles.clear();
 	for (auto i : r)
 	{
 		SoldierRole role = static_cast<SoldierRole>(i);
