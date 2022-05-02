378331e643c458e70e8a4bfa72e9e6354b00efa3
Finnik <45336117+Finnik723@users.noreply.github.com>
2022-05-03 00:49:51 +0300

    fix for #138

--- src/Geoscape/GeoscapeState.cpp
+++ src/Geoscape/GeoscapeState.cpp
@@ -3037,7 +3037,7 @@ void GeoscapeState::btnFundingClick(Action *)
 	{
 		return;
 	}
-	if (Options::oxceLinks)
+	if (Options::oxceLinks || _game->getMod()->getIsFTAGame())
 	{
 		_game->pushState(new ExtendedGeoscapeLinksState(this));
 	}
