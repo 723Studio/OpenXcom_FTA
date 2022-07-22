03da7cec8eae05dfd88b578f903895762b981040
Finnik <45336117+Finnik723@users.noreply.github.com>
2022-07-22 22:11:57 +0300

    fixes to manufacture states

--- src/Savegame/Soldier.cpp
+++ src/Savegame/Soldier.cpp
@@ -764,10 +764,9 @@ void Soldier::setCraftAndMoveEquipment(Craft* craft, Base* base, bool isNewBattl
  */
 std::string Soldier::getCurrentDuty(Language *lang, const BaseSumDailyRecovery &recovery, bool &isBusy, bool &isFree, DutyMode mode) const
 {
-	//std::string s;
 	isBusy = false;
 	isFree = false;
-	bool facility = (mode == LAB || mode == ASSIGN);
+	bool facility = (mode == LAB || mode == ASSIGN || mode == WORK);
 	if (_death)
 	{
 		if (_death->getCause())
